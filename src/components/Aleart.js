1f80f0dc82303ba3ec254b6fbc5c617e2a1d0bfd
youcanalsocode <nehan.gupta12@gmail.com>
2024-05-02 15:38:09 +0530

    Addition of favicons to notes

---
+++ src/components/Aleart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Aleart() {
+  return (
+    <div>
+      <div class="alert alert-primary" role="alert">
+        This is a primary alert—check it out!
+      </div>
+    </div>
+  );
+}
